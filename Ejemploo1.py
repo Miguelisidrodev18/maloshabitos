15f114ffdff89707dcf430db3dcf8cfa26387063
Miguelisidrodev18 <74093841@continental.edu.pe>
2024-04-10 17:19:55 -0500

    Ya esta corregido el 1

--- Ejemploo1.py
+++ Ejemploo1.py
@@ -2,16 +2,11 @@ def multiplicacion(m1, m2):
     result = m1 * m2
     return result
 
-
-
-
 primer_numero =10
 segundo_numero=5
-print("El primer numero es: ", primer_numero)
-print("El segundo numero es:", segundo_numero)
-
 t = primer_numero + segundo_numero
-
+print("El primer numero es:", primer_numero)
+print("El segundo numero es:", segundo_numero)
 
 resultado = multiplicacion(primer_numero, t)
 print("El resultado de la operacion es:")
