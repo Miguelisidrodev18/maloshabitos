0975730c4cc3002b35af9dc58dbeda43d1a08bdb
Miguelisidrodev18 <74093841@continental.edu.pe>
2024-04-10 17:35:31 -0500

    Ya esta arreglado0 el 2

--- Ejemploo2.py
+++ Ejemploo2.py
@@ -1,12 +1,12 @@
-def calcular(a, b, c):
-    res = a * b + c
+def calcular(VARIABLE1, VARIABLE2, VARIABLE3):
+    res = VARIABLE1 * VARIABLE2 + VARIABLE3
     return res
 
 def principal():
-    x = 5
-    y = 3
-    z = 7
-    resultado = calcular(x, y, z)
+    condicion1 = 5
+    Condicion2 = 3
+    Condicion3 = 7
+    resultado = calcular(condicion1, Condicion2, Condicion3)
     print("El resultado es:", resultado)
 
 principal()
